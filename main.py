972828602a6acef2e34182d5b6dde211215feace
otaviobaroni <otaviobaroni223@gmail.com>
2023-02-27 17:46:08 -0300

    First Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from pessoa import Pessoa
+
+
+p1 = Pessoa('Luiz', 29)
+p1.comer('maçã')
+p1.falar("POO")
+#p1.nome = "Luiz"
+#mesma coisa
---
+++ pessoa.py
@@ -0,0 +1,38 @@
+class Pessoa:
+    def __init__(self, nome, idade, comendo=False, falando=False):
+        self.nome = nome  # recebe o valor do main
+        self.idade = idade
+        self.comendo = comendo
+        self.falando = falando
+
+    def falar(self, assunto):
+        if self.comendo:
+            print(f'{self.nome} não pode falar comendo.')
+            return
+
+        if self.falando:
+            print(f'{self.nome} ja está falando.')
+            return
+
+            print(f'{self.nome} está falando.')
+            self.falando = True
+
+
+    def comer(self, alimento):
+        if self.comendo:
+            print(f'{self.nome} ja está comendo.')
+            return
+
+        print(f'{self.nome} está comendo {alimento}.')
+        self.comendo = True
+
+
+    def parar_comer(self):
+        if not self.comendo:
+            print(f'{self.nome} não está comendo.')
+            return
+
+        print(f'{self.nome} parou de comer.')
+        self.comento = False
+
+
